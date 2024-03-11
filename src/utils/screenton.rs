9fcc408825acdf14d49b99d9330672980f10d403
umzi <ab5447612@gmail.com>
2024-03-12 00:48:16 +0300

    speed up

--- src/utils/screenton.rs
+++ src/utils/screenton.rs
@@ -24,30 +24,36 @@ impl Screenton {
         }
     }
     pub fn run(&mut self, array: &mut Array2<f32>) {
-
+        let min:f32 = 0.0;
+        let max:f32 = 1.0;
         let (height,width)=(array.shape()[0],array.shape()[1]);
 
         for ly in 0..height {
-            for lx in 0..width {
-                let lx2 = lx+self.lx_plus;
-                let ly2 = ly+self.ly_plus;
-                let row = lx2 / self.dot_size;
-                let column = ly2 / self.dot_size;
-                let i = lx2 % self.dot_size;
-                let j = ly2 % self.dot_size;
+            let ly2 = ly+self.ly_plus;
+            let j = ly2 % self.dot_size;
+            let column = ly2 / self.dot_size;
 
-                let src: ArrayView2<f32> = if (row + column) % 2 == 1 {
-                    self.dot_inv.view()
-                } else {
-                    self.dot.view()
-                };
-                if array[[ly, lx]] < src[[i,j]] {
-                    array[[ly, lx]] = 0.0;
-                } else {
-                    array[[ly, lx]] = 1.0
+            for lx in 0..width {
+                let img_volume:f32 = array[[ly,lx]];
+                if img_volume < max && img_volume > min {
+                    let lx2 = lx+self.lx_plus;
+                    let row = lx2 / self.dot_size;
+                    let i = lx2 % self.dot_size;
+
+                    let src: ArrayView2<f32> = if (row + column) % 2 == 1 {
+                        self.dot_inv.view()
+                    } else {
+                        self.dot.view()
+                    };
+                    if img_volume < src[[i,j]] {
+                        array[[ly, lx]] = 0.0;
+                    } else {
+                        array[[ly, lx]] = 1.0
+                    }
                 }
             }
         }
-
     }
+
 }
+
